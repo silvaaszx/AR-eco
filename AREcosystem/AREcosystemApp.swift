8dfa364d83c507399aeadeb441b53334680342ff
Matheus <blackgg100500@gmail.com>
2025-06-11 17:03:50 -0300

    Initial Commit

---
+++ AREcosystem/AREcosystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  AREcosystemApp.swift
+//  AREcosystem
+//
+//  Created by Matheus Silva on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AREcosystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AREcosystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AREcosystem
+//
+//  Created by Matheus Silva on 11/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
